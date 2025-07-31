8288c333be56cbcb9af3635b92484b95de2f895b
user-Zarin <xarinkhan45@gmail.com>
2025-07-31 18:23:17 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate ,BrowserRouter,Route,Routes} from "react-router-dom";
 import Home from './Components/Home';
-import Sura from "./Components/sura"
+import Sura from "./Components/Sura"
 const App = () => {
   return (
     <div>
      <BrowserRouter>
      <Routes>
-      <Route path='/' element={<Home/>}> </Route>
+      <Route path='/Holy-Scripture-Website' element={<Home/>}> </Route>
       <Route path="/Surah/:id" element={<Sura />} />
      </Routes>
      </BrowserRouter>
